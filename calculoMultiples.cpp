18db0709e7f716c0fa7fa39a04c2e6dc04e8b2b0
Mikel Arina Marcos <mikel.arina.marcos@gmail.com>
2023-05-06 08:00:13 +0200

    Terminado ejercicio contar cantidad de múltiplos

--- calculoMultiples.cpp
+++ calculoMultiples.cpp
@@ -5,20 +5,25 @@
 using namespace std;
 
 int main() {
-	int a, b, k; // Declaramos las tres variables que vamos a usar.
-	cout << "Introduce un número mayor o igual a 0: ";
+	int a, b, k, r; // Declaramos las tres variables que vamos a usar.
+	cout << "Introduce el limite inferior del rango de valores (mayor o igual que 0): ";
 	cin >> a;
-	cout << "Introduce otro número mayor o igual a 0: ";
+	cout << "Introduce el limite superior del rango de valores (mayor o igual que 0): ";
 	cin >> b;
-	cout << "Introduce el número que quieres compara si es múltiple o no: ";
+	cout << "Introduce el numero del cual quieres saber cuantos multiplos hay: ";
 	cin >> k;
-	
+	r = 0;
 	if (a < 0 || b < 0 || k <= 0 || a > b) {
 		cout << "Error en alguno de los datos. Introducelos de nuevo.";
 		return 0;
 	}
 	else {
-		cout << "Vas bien.";
+		for (int i = a; i <= b; i++) {
+			if (i % k == 0) {
+				r++;
+			}
+		}
+		cout << "Hay " << r << " multiplos de " << k << " entre " << a << " y " << b;
 		return 0;
 	}
 }
